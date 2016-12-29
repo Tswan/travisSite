5a07376afc81795ac9839b2ad8f298aeb5cc57bc
Travis Swan <t_r_z_swan@hotmail.com>
2016-12-29 15:43:55 -0500

    Trying to fix masonry.js script loading bug, masonry seems to be loaded before the DOM is loaded

--- dist/js/travis-site.js
+++ dist/js/travis-site.js
@@ -3352,56 +3352,66 @@ return Outlayer;
 }));
 ;
 
-var msnry = new Masonry( '.container', {
-    columnWidth: '.item.w2',
-    percentPosition: true,
-    "gutter": 5 
-});
+//$(document).ready(setTimeout(function() {gridInit(); }, 1000));
+$(document).ready(function() {gridInit();});
+/*if (window.addEventListener)
+  window.addEventListener("load", gridInit(), false);
+else if (window.attachEvent)
+  window.attachEvent("onload", gridInit());
+else window.onload = gridInit();*/
 
-function getHref(href) {
-  return (href.indexOf('youtube') == -1 ? 'image' : 'iframe');
-} 
-
-$(".fancybox").click(function () {
-  if (!this.nextSibling) {
-    return;
-  }
-
-  $('.item').fancybox({
-    'padding'       : 0,
-    'autoScale'     : true,
-    'autoDimensions': true,
-    'fitToView'       : true,
-    'transitionIn'  : 'none',
-    'transitionOut' : 'none',
-    prevEffect : 'none',
-    nextEffect : 'none',
-    arrows : true,
-    'title'         : this.title,
-    'swf'           : {
-    'wmode'        : 'transparent',
-    'allowfullscreen'   : 'true'
-    },
-    helpers : {
-    media : {},
-    buttons : {}
-    },beforeLoad : function(){
-    this.type = getHref(this.href);
-      if(this.type != 'image') 
-      {
-        this.width = $(window).width();
-        this.height = $(window).width()/2;
-      }
-    },afterLoad :
-    function (){
-      if(this.type != 'image') 
-      {
-        this.width = $(window).width();
-        this.height = $(window).width()/2;
-      }
-    }
+function gridInit() {
+  var msnry = new Masonry( '.container', {
+      columnWidth: '.item.w2',
+      percentPosition: true,
+      "gutter": 5 
   });
-});;
+
+  function getHref(href) {
+    return (href.indexOf('youtube') == -1 ? 'image' : 'iframe');
+  } 
+
+  $(".fancybox").click(function () {
+    if (!this.nextSibling) {
+      return;
+    }
+
+    $('.item').fancybox({
+      'padding'       : 0,
+      'autoScale'     : true,
+      'autoDimensions': true,
+      'fitToView'       : true,
+      'transitionIn'  : 'none',
+      'transitionOut' : 'none',
+      prevEffect : 'none',
+      nextEffect : 'none',
+      arrows : true,
+      'title'         : this.title,
+      'swf'           : {
+      'wmode'        : 'transparent',
+      'allowfullscreen'   : 'true'
+      },
+      helpers : {
+      media : {},
+      buttons : {}
+      },beforeLoad : function(){
+      this.type = getHref(this.href);
+        if(this.type != 'image') 
+        {
+          this.width = $(window).width();
+          this.height = $(window).width()/2;
+        }
+      },afterLoad :
+      function (){
+        if(this.type != 'image') 
+        {
+          this.width = $(window).width();
+          this.height = $(window).width()/2;
+        }
+      }
+    });
+  });
+} ;
 /*!
  * fancyBox - jQuery Plugin
  * version: 2.1.5 (Fri, 14 Jun 2013)
